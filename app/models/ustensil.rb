a46948003c97d1e885deb6100ca94c015ff21b63
maxence maho <maxencemaho@gmail.com>
2025-11-25 18:01:11 +0100

    Wait to add unique name for ustantil name

--- app/models/ustensil.rb
+++ app/models/ustensil.rb
@@ -2,6 +2,7 @@ class Ustensil < ApplicationRecord
   belongs_to :user
   has_many :fridge, through: :user
   validates :name, presence: true
+  validates :name, uniqueness: true
   USTENSIL = ["plaque chauffantes", "four"]
   validates :name, inclusion: { in: USTENSIL }
 end
