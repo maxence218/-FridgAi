1c0044952904a2a634ae118142c7431b0bb38be1
maxence maho <maxencemaho@gmail.com>
2025-11-25 17:16:36 +0100

    routes set up

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :ustensils
   resources :recipes
+  resources :ustensils
 
   resources :fridges, only: :index do
     resources :ingredients
