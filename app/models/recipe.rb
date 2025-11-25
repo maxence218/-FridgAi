715e1734c56965eaf9aea91c5092a5ac0e7cf068
Zoonekynd Alexandre <alexzoonekynd@gmail.com>
2025-11-25 11:27:07 +0100

    fix syntax  belongs_to to recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,5 +2,5 @@ class Recipe < ApplicationRecord
   belongs_to :chats
   belongs_to :users
   has_many :messages, through: :chats
-  belong_to :fridge, through: :users
+  belongs_to :fridge, through: :users
 end
