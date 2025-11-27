71d58cc6a2d110fa211601ba1f6ab7744b03446c
maxence maho <maxencemaho@gmail.com>
2025-11-27 15:37:16 +0100

    user got a name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :initialize_fridge
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -10,5 +11,11 @@ class ApplicationController < ActionController::Base
         current_user.fridge = Fridge.create(user: current_user)
       end
     end
+  end
+
+   protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :diet, :allergies])
   end
 end
