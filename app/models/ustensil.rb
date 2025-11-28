71eec8ef1d223c56a25111d256ca041f0af19d0f
Adam Labruyere <adam.labruyere@gmail.com>
2025-11-28 16:58:48 +0100

    validates

--- app/models/ustensil.rb
+++ app/models/ustensil.rb
@@ -1,7 +1,6 @@
 class Ustensil < ApplicationRecord
   belongs_to :fridge
   validates :name, presence: true
-  validates :name, uniqueness: true
   USTENSIL = ["Oven", "Beater", "Blender", "Pan", "Stove", "Pot"]
   validates :name, inclusion: { in: USTENSIL }
 end
