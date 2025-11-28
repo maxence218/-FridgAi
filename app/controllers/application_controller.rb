b35b6e2cc6db11ade7d841b6ddf9c1132ecff6f5
maxence maho <maxencemaho@gmail.com>
2025-11-28 16:07:11 +0100

    initialiaze fridge at user creation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,15 +5,7 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def initialize_fridge
-    unless current_user.nil?
-      if current_user.fridge.nil?
-        current_user.fridge = Fridge.create(user: current_user)
-      end
-    end
-  end
-
-   protected
+  protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :diet, :allergies])
